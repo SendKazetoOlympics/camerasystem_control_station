5ffa70c8266eaf24a372bb521b81794709e54193
Kaze Wong <kazewong.physics@gmail.com>
2025-01-01 16:15:09 -0500

    initial setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- Make a text field that take text as ip input and set a button to send a request to it -->
+
+<script>
+    import { onMount } from 'svelte';
+    onMount(() => {
+        const ping = document.getElementById('ping');
+        const ip = document.getElementById('ip');
+        ping.addEventListener('click', () => {
+        fetch(`http://localhost:5000/start`)
+            .then(res => res.json())
+            .then(data => {
+            console.log(data.message);
+            })
+            .catch(err => {
+            console.log(err);
+            });
+        });
+    });
+</script>
+
+
+<input type="text" id="ip" placeholder="Enter IP Address">
+<button id="ping">Ping</button>
