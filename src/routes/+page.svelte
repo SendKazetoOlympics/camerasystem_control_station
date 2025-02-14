9b7ff14f9c333813ea7ca516f8b149c004b5a0d8
Kaze Wong <kazewong.physics@gmail.com>
2025-02-13 19:51:20 -0500

    update ip function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,32 @@
 <!-- Make a text field that take text as ip input and set a button to send a request to it -->
 
-<script>
+<script lang="ts">
     import { onMount } from 'svelte';
-    onMount(() => {
-        const ping = document.getElementById('ping');
-        const ip = document.getElementById('ip');
-        ping.addEventListener('click', () => {
-        fetch(`http://localhost:5000/start`)
-            .then(res => res.json())
-            .then(data => {
-            console.log(data.message);
-            })
-            .catch(err => {
-            console.log(err);
-            });
-        });
-    });
+
+    let ips = $state([]);
+
+    function addIP() {
+        const ip = document.getElementById('ip').value;
+        ips.push(ip);
+        console.log(ips);
+    }
+
+
 </script>
 
+<!-- Add two buttons, one for adding an entry for IP and another for deleting -->
+<button class='btn' onclick={addIP}>Add</button>
+<button class='btn' id="delete">Delete</button>
 
+<!-- Add a text field for IP address -->
 <input type="text" id="ip" placeholder="Enter IP Address">
+
+<!-- Add a button for pinging the IP address -->
 <button id="ping">Ping</button>
+
+<!-- Display the list of IP addresses -->
+<ul>
+    {#each ips as ip}
+        <li>{ip}</li>
+    {/each}
+</ul>
