f877d1dc8b723cab8060548775e9a11f7ae7d154
Kaze Wong <kazewong.physics@gmail.com>
2025-12-09 09:12:41 -0800

    Add bun.lock, enable DaisyUI and bump deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,12 @@
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
 
-	plugins: [typography, forms, require('daisyui')]
+	plugins: [typography, forms, daisyui],
+	
 } satisfies Config;
